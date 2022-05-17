dd1de36c5a25b1c3a9693ae3187c92fb380a7be4
Mahmoud Majid <76546155+Mahmoud-Majid@users.noreply.github.com>
2022-05-17 15:44:12 +0200

    update

--- graphql/graphql.js
+++ graphql/graphql.js
@@ -1,11 +1,11 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 let articles = [
-    { title: "A1", body: "B1" }, 
-    { title: "A2", body: "B2" }, 
-    { title: "A3", body: "B3" }, 
-    { title: "A4", body: "B4" }, 
-    { title: "A5", body: "B5" }, 
+    { title: "A1", body: "B1" },
+    { title: "A2", body: "B2" },
+    { title: "A3", body: "B3" },
+    { title: "A4", body: "B4" },
+    { title: "A5", body: "B5" },
 ];
 
 // schema
@@ -39,28 +39,26 @@ const resolvers = {
         },
 
         deleteArticle: (_, args, ctx) => {
-            console.log("===============");
 
             console.log(args);
             const result = articles.filter(art => art.title !== args.title);
-            console.log("===============");
             articles = result;
             return result;
         }
 
-        
+
     },
     Query: {
         allArticles: (_, args, ctx) => {
-            const result = articles.slice(-args.last);            
+            const result = articles.slice(-args.last);
             return [...result];
         }
     }
 }
 
 
-const server = new ApolloServer({ 
+const server = new ApolloServer({
     typeDefs,
     resolvers
- });
+});
 server.listen(3001).then(({ port }) => { console.log('listening on: ', port) });
